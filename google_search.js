b0ee5bcc3386618bcbb382749d6d3bd4a852f4d0
Will Hurley <wjhurley@users.noreply.github.com>
2017-01-05 14:41:35 -0800

    upload sample file and README

---
+++ google_search.js
@@ -0,0 +1,27 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(check_title, 1000);
+
+function check_title() {
+  var promise = driver.getTitle().then( function(title) {
+    if (title === 'wiki - Google Search' )
+    {
+      console.log('success');
+      return true;
+    }
+    else
+    {
+      console.log('fail -- ' + title);
+    }
+  });
+  return promise;
+}
